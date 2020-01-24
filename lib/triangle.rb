63215253fa77cbf97dfea49e63519140df34b734
Meghan Flynn <mflynn333@gmail.com>
2020-01-24 21:28:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,8 +14,7 @@ class Triangle
     #The sum of the lengths of any two sides of a triangle always exceeds the length of the third side.
     
     if (@s1 + @s2) <= @s3 || (@s2 + @s3) <= s1 || (@s1 + @s3) <= @s2
-      
-      #raise 
+      raise TriangleError 
     
   end 
   

a3af89d94a4aed8e47b99954ab1dbb9a0b4d0131
Meghan Flynn <mflynn333@gmail.com>
2020-01-24 21:16:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :equilateral, :isosceles, :scalene 
+  
+  def initialize (s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end 
+  
+  def kind 
+  end 
+  
+class TriangleError < StandardError 
+end 
+
 end
